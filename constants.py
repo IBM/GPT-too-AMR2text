ddf798770594fd6828aad5b9fb40b6103c9021d0
pywirrarika <pywirrarika@gmail.com>
2020-05-17 20:51:53 +0200

    Commiting constants

--- constants.py
+++ constants.py
@@ -1,3 +1,11 @@
-SPECIAL_TOKENS = ["<bos>", "<eos>", "<context>", "<answer>", "<question>", "<pad>"]
-MODEL_INPUTS  = ["input_ids", "labels", "token_type_ids"]
-PADDED_INPUTS = ["input_ids", "labels", "token_type_ids"]
+SPECIAL_TOKENS = ["<bos>", "<eos>", "<context>", "<answer>", "<question>",
+                  "<pad>", "<generate>"]
+SPECIAL_TOKENS_BERT = ["[CLS]", "[SEP]", "[PAD]", "[unused0]"]
+MODEL_INPUTS = ["input_ids", "labels", "token_type_ids", "attention_mask"]
+PADDED_INPUTS = ["input_ids", "labels", "token_type_ids", "attention_mask"]
+OTHERS = ["<unk2>"]
+AMR_SPECIAL_TOKENS = ['multi-sentence', '<join>', ':snt1', ':snt2',
+                      ':snt3', ':snt4', ':snt5', ':snt5', ':snt6', ':snt7',
+                      ':snt8', ':snt9']
+
+SPECIAL_TOKENS = SPECIAL_TOKENS
